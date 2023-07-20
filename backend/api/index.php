6a2882edd3c2cbc4569e030005fdaa06baff35de
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-20 14:07:47 +0200

    Add call from data.json

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,5 +1,7 @@
 <?php
-$tasks = ["Comprare le bistecche", "Prendere da Bere", "Pulire casa", "Preparare la brace", "Cucinare"];
+$database_url = __DIR__ . '../../database/data.json';
+$json = file_get_contents($database_url);
+$tasks = json_decode($json, true);
 
 header('Content-Type: application/json');
 echo json_encode($tasks);
